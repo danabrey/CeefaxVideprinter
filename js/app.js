80ec33ff97307e321b2d1f56191aa2de222cfd6c
Dan Abrey <danabrey@gmail.com>
2018-07-11 15:07:52 +0100

    Updates

--- js/app.js
+++ js/app.js
@@ -429,20 +429,21 @@ CeefaxVideprinter.filter('cut', function () {
 ;CeefaxVideprinter.controller('VideprinterCtrl', ['$scope', '$http', '$interval', "PageData", function($scope, $http, $interval, PageData) {
     var limit = 15;
     var refresh = function() {
-        var url = "//polling.bbc.co.uk/sport/shared/football/videprinter.json?callback=JSON_CALLBACK";
-        $http.jsonp(url)
+        var url = "https://sportinglife.com/api/football/vidiprinter";
+        $http.json(url)
             .success(function(data){
-                var totalEvents = data.data.payload.events.length;
-                if (totalEvents > 0) {
-                    $scope.videprinterEvents = [];
-                    angular.forEach(data.data.payload.events.reverse().slice(0,limit-1), function(val, key) {
-                        if (val.label == 'FULL-TIME') val.label = 'FT';
-                        if (val.label == 'HALF-TIME') val.label = 'HT';
-                        if (val.label == 'GOAL!') val.label = 'GOAL';
-                        if (val.label == 'RED CARD!') val.label = 'RED';
-                        $scope.videprinterEvents.push(val);
-                    });
-                }
+                console.log(data);
+                // var totalEvents = data.data.payload.events.length;
+                // if (totalEvents > 0) {
+                //     $scope.videprinterEvents = [];
+                //     angular.forEach(data.data.payload.events.reverse().slice(0,limit-1), function(val, key) {
+                //         if (val.label == 'FULL-TIME') val.label = 'FT';
+                //         if (val.label == 'HALF-TIME') val.label = 'HT';
+                //         if (val.label == 'GOAL!') val.label = 'GOAL';
+                //         if (val.label == 'RED CARD!') val.label = 'RED';
+                //         $scope.videprinterEvents.push(val);
+                //     });
+                // }
             });
     }
     refresh();
